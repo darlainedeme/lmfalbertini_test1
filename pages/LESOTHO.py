e22dc2d85b421e0391973dbc531dfee5024c5fdd
Darlain Edeme <45342315+darlainedeme@users.noreply.github.com>
2022-08-03 17:18:32 +0200

    pdf

--- pages/LESOTHO.py
+++ pages/LESOTHO.py
@@ -13,6 +13,13 @@ url4 = 'sdgs.un.org/goals/goal7'
 if st.button('SDG 7'):
     webbrowser.open_new_tab(url4)
 
+st.write(str(os.getcwd()))
+
 with open("layout.pdf", "rb") as pdf_file:
     PDFbyte = pdf_file.read()
-    st.write(str(os.getcwd()))
+    
+
+    st.download_button(label="Download FAC-SIMILE",
+                       data=PDFbyte,
+                       file_name="test.pdf",
+                       mime='application/octet-stream')
